4273045f751696ee2f27f1ace96a3c5d6e443c5c
张瑞 <rui.zhang07@hand-china.com>
2019-03-18 20:34:27 +0800

    01

---
+++ spring01/src/main/java/pers/zr/spring/HelloComponant.java
@@ -0,0 +1,10 @@
+package pers.zr.spring;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class HelloComponant {
+    public String hello(){
+        return "I am hello";
+    }
+}
